a82e1a0a7cdeeba76207d893e7e657d5164ec73e
OleksandrShevchenko <shevchenko2106@gmail.com>
2022-09-18 11:26:54 +0300

    hw1-part2

--- src/components/characters/Character.js
+++ src/components/characters/Character.js
@@ -1,13 +1,21 @@
-function Character(props){
-    const {character} = props;
+function Character(props) {
+    const {character} =props;
     return(
         <div>
             <h3> {character.id}--{character.name}</h3>
-            <p>{character.status}</p>
+            <h5> {character.status}</h5>
+            <h5> {character.species}</h5>
+            <h5> {character.type}</h5>
+            <h5> {character.gender}</h5>
+            <h5> {character.origin.name}</h5>
+            <h5> {character.origin.url}</h5>
+            {/*<h5> {character.location}</h5>*/}
             <img src = {character.image} alt = {character.image}/>
+            <h5> {character.episode}</h5>
+            <h5> {character.url}</h5>
+            <h5> {character.created}</h5>
 
         </div>
     );
 }
-
 export default Character;
--- src/components/characters/Characters.js
+++ src/components/characters/Characters.js
@@ -3,19 +3,19 @@ import Character from "./Character";
 
 function Characters(){
 
-    const [characters,setCharacters] = useState([]);
+    const[characters, setCharacters] = useState([]);
 
     fetch('https://rickandmortyapi.com/api/character')
         .then(value => value.json())
-        .then(value =>{
+        .then(value => {
             setCharacters(value.results.splice(0,6));
         });
-    return (
+    return(
         <div>
             {
-            characters.map (character => <Character key={character.id}
-            character = {character}
-            />)
+                characters.map(character => <Character key={character.id}
+                character = {character}
+                />)
             }
         </div>
     );
